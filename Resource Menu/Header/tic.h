c3e291f8f6bde0c05c7e51d2dbb42ef94cf91b8f
Mahmoudbannouri <100564826+Mahmoudbannouri@users.noreply.github.com>
2022-05-07 12:17:08 +0100

    Add files via upload

---
+++ Resource Menu/Header/tic.h
@@ -0,0 +1,32 @@
+#ifndef ENTETE_H_INCLUDED
+#define ENTETE_H_INCLUDED
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include <SDL/SDL_ttf.h>
+typedef struct
+{
+int joueur;
+SDL_Surface * boxes,*xs,*os;
+int tabsuivi[9];
+SDL_Rect pos;
+int tour;
+TTF_Font *font;
+SDL_Color couleur;
+SDL_Rect postxt;
+}tic;
+//
+void initialisation(tic *t);
+
+void affichage(tic t,SDL_Surface *ecran ) ;
+
+int atilganer(int tabsuivi[]);
+
+int minimax(int tabsuivi[],int joueur);
+
+void calcul_coup(int tabsuivi[]);
+
+void Resultat (tic t,SDL_Surface* ecran);
+
+void liberationmemoire(tic t);
+
+#endif // ENTETE_H_INCLUDED
