493699a6c99b069490c69d356971ced02faae8d8
widaskri <101092798+widaskri@users.noreply.github.com>
2022-05-08 08:53:28 +0100

    Update main.c

--- Resource Back/main.c
+++ Resource Back/main.c
@@ -6,6 +6,18 @@
 #include "collisionparfaite.h"
 #include "background.h"
 
+/**
+ *@file main.c
+ *@brief testing program.
+ *@author wided
+ *@version 0.1
+ *@date apr 24,2022
+ *
+ *testing program for background scrolling
+ *
+ */
+
+
 int main(void)
 {	
 	int continuer=1;
@@ -47,7 +59,7 @@ int main(void)
 			case SDLK_RIGHT:
 			 
 			if ( collisionparfaite(screen,perso)==10){
-				scrolling(&ba,0);	//scrollingymin;	
+				//scrolling(&ba,2);	//scrollingymin;	
 				scrolling(&bm,0);
 			}
 					
@@ -55,7 +67,7 @@ int main(void)
                         case SDLK_LEFT:
                     
                        if ( collisionparfaite(screen,perso)==10){
-				scrolling(&ba,1);//scrolllingysar;
+				//scrolling(&ba,1);//scrolllingysar;
 				scrolling(&bm,1);
 			}			
 		 
@@ -63,17 +75,36 @@ int main(void)
 			 case SDLK_UP:
 			 
 		  		if ( collisionparfaite(screen,perso)==10){
-					scrolling(&ba,2);	//scrollingymin;	
-					scrolling(&bm,2);
+					//scrolling(&ba,4);	//scrollingymin;	
+					scrolling(&bm,4);
 				}			 
                         break;
  			 case SDLK_DOWN:
  			 
  		  		if ( collisionparfaite(screen,perso)==10){
-					scrolling(&ba,3);	//scrollingymin;	
-					scrolling(&bm,3);
+					//scrolling(&ba,5);	//scrollingymin;	
+					scrolling(&bm,5);
 				}			 
-                        break;	
+                        break;
+                          case SDLK_d:
+                               if ( collisionparfaite(screen,perso)==10)
+                                      scrolling(&ba,2);
+                        break;
+                           case SDLK_q:
+                               if ( collisionparfaite(screen,perso)==10)
+                                      scrolling(&ba,3);
+                        break;
+                           case SDLK_z:
+                               if ( collisionparfaite(screen,perso)==10)
+                                      scrolling(&ba,6);
+                        break;
+                           case SDLK_s:
+                               if ( collisionparfaite(screen,perso)==10)
+                                      scrolling(&ba,7);
+                        break;
+
+
+	
       		 }
       		 	
             break;
