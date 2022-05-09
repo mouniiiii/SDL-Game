791568c8537f44dc408029de42c9caa462b5a6b8
Mahmoudbannouri <100564826+Mahmoudbannouri@users.noreply.github.com>
2022-05-09 12:42:40 +0200

    Update tic.c

--- tic.c
+++ tic.c
@@ -75,7 +75,7 @@ void liberertic(tic t)
 
 int atilganer(int tabsuivi[])
 {
-    // remplir cette matrice avec les combinaisons de succÃ©s
+    
     int lignes_gagnantes[8][3] = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
     int i;
     for (i = 0; i < 8; ++i)
